80522d7398d87ea1197e2c06605779adf610db77
chronnick <nicholasadamnelson@gmail.com>
2022-05-20 21:49:22 -0400

    asd

---
+++ Symlinks/Redirect WOW settings to Onedrive.bat
@@ -0,0 +1,3 @@
+mklink /j "C:\Games\World of Warcraft\_retail_\Interface" "C:\Users\nick\OneDrive\Documents\Games\WOW\Interface"
+
+mklink /j "C:\Games\World of Warcraft\_retail_\WTF" "C:\Users\nick\OneDrive\Documents\Games\WOW\WTF"
