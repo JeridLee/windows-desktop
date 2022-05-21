607c6be7308021374758733c0bb65d4c4ece4245
chronnick <nicholasadamnelson@gmail.com>
2022-05-20 22:23:39 -0400

    update

---
+++ Scripts/Redirect WOW settings to Onedrive.bat
@@ -0,0 +1,3 @@
+mklink /j "C:\Games\World of Warcraft\_retail_\Interface" "C:\Users\nick.CHRON\OneDrive - chronnick\Documents\My Games\WOW\Interface"
+
+mklink /j "C:\Games\World of Warcraft\_retail_\WTF" "C:\Users\nick.CHRON\OneDrive - chronnick\Documents\My Games\WOW\WTF"
--- Symlinks/Redirect WOW settings to Onedrive.bat
+++
@@ -1,3 +0,0 @@
-mklink /j "C:\Games\World of Warcraft\_retail_\Interface" "C:\Users\nick\OneDrive\Documents\Games\WOW\Interface"
-
-mklink /j "C:\Games\World of Warcraft\_retail_\WTF" "C:\Users\nick\OneDrive\Documents\Games\WOW\WTF"
