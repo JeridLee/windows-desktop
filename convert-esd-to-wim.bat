b4ae66109c3083f1c46f2fba9f13c52d51f3dae4
Nick Nelson <nicholasadamnelson@gmail.com>
2022-05-27 01:59:37 -0400

    Update convert-esd-to-wim.bat

--- convert-esd-to-wim.bat
+++ convert-esd-to-wim.bat
@@ -1,2 +1,10 @@
+#windows-11
 dism /Get-WimInfo /WimFile:install.esd
-dism /export-image /SourceImageFile:install.esd /SourceIndex:# /DestinationImageFile:C:\WIM\Win10-Home-20H2.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:1 /DestinationImageFile:Win11-install-HOME.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:2 /DestinationImageFile:Win11-install-HOME-N.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:3 /DestinationImageFile:Win11-install-HOME-SL.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:4 /DestinationImageFile:Win11-install-EDU.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:5 /DestinationImageFile:Win11-install-EDU-N.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:6 /DestinationImageFile:Win11-install-PRO.wim /Compress:max /CheckIntegrity
+dism /export-image /SourceImageFile:Win11-install.esd /SourceIndex:7 /DestinationImageFile:Win11-install-PRO-N.wim /Compress:max /CheckIntegrity
+#windows-10
