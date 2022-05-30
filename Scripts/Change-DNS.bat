7421e32a2849f210a32a6d3414cdc0dda91343b0
Nick Nelson <nicholasadamnelson@gmail.com>
2022-05-30 12:11:32 -0400

    Update Change-DNS.bat

--- Scripts/Change-DNS.bat
+++ Scripts/Change-DNS.bat
@@ -2,7 +2,7 @@
 :: ipconfig | find /I "Ethernet adapter"
 :: replace "Ethernet" below with the name of your adapter
 
-echo set DNS to DHCP
+echo set DNS to Cloudflare
 netsh interface ipv4 set dns "Ethernet" static 1.1.1.2
 netsh interface ipv4 add dns "Ethernet" 1.0.0.2 index=2
 
