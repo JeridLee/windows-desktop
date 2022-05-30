b0d6dd0fa2c09e7802b35e355b1e9bb0690af98c
Nick Nelson <nicholasadamnelson@gmail.com>
2022-05-30 00:30:00 -0400

    Create Change-DNS.bat

---
+++ Scripts/Change-DNS.bat
@@ -0,0 +1,12 @@
+# find name of ethernet adapter
+# ipconfig | find /I "Ethernet adapter"
+# replace "Ethernet" below with the name of your adapter
+
+# set DNS to DHCP
+netsh interface ipv4 set dns "Ethernet" static 1.1.1.2
+netsh interface ipv4 add dns "Ethernet" 1.0.0.2 index=2
+
+pause
+
+# set DNS back to DHCP
+netsh interface ipv4 set dns "Ethernet" dhcp
